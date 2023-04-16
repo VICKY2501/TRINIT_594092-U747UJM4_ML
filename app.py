6ad9c35707ee04d118b6e094ff98cf473eff75c5
Vaibhav gupta <77627439+VICKY2501@users.noreply.github.com>
2023-04-16 23:13:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def allZero(arr):
 # import model:
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import classification_report
 from sklearn import metrics
